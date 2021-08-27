9e1fd4c9ab609dfcaa6891f155691b0008c2cc9c
BrianEAC <brianatariaceraldi@gmail.com>
2021-08-27 15:38:20 -0300

    front-end 0.1. first mockup

--- stingy/src/App.js
+++ stingy/src/App.js
@@ -9,11 +9,12 @@ const SubmitForm = ({
   submit,
   handleChange,
   checked,
+  thisClass,
 }) => {
   return (
-    <div>
-      <form onSubmit={submit}>
-        <div>
+    <div className="formButton">
+      <form onSubmit={submit} className={thisClass}>
+        <div className="radio">
           <input
             type="radio"
             id="radio-one"
@@ -32,7 +33,7 @@ const SubmitForm = ({
           <label htmlFor="radio-two">Excome </label>
         </div>
         <div>
-          $
+          Amount: $
           <input
             value={moneyNumber}
             onChange={moneyChange}
@@ -44,8 +45,7 @@ const SubmitForm = ({
           />
         </div>
         <div>
-          Description:{" "}
-          <input value={typeName} onChange={typeChange} required/>
+          Description: <input value={typeName} onChange={typeChange} required />
         </div>
         <button type="submit">add</button>
       </form>
@@ -55,30 +55,32 @@ const SubmitForm = ({
 
 const Movements = ({ movementType, className }) => {
   return movementType.map((mov) => (
-    <div className={className}>
-      description: {mov.description}$:{mov.value} type: {mov.type}
-    </div>
+    <ul className={className}>
+      <h3>{mov.description}</h3> <li>${mov.value} </li>
+      <li>{mov.type}</li>
+    </ul>
   ));
 };
 
 const Total = ({ incomes, expenses }) => {
   return (
-    <>
-      <ul>
+    <div>
+      <ul className="total">
         <li>Incomes: {incomes}</li>
         <li>Expenses: {expenses}</li>
         <li>Total: {incomes - expenses}</li>
       </ul>
-    </>
+    </div>
   );
 };
 
 function App() {
+  const [movements, setMovements] = useState([]);
   const [incomes, setIncome] = useState([]);
   const [expenses, setExpense] = useState([]);
   const [expenseName, setExpenseName] = useState("");
   const [money, setMoney] = useState("");
-  const [isChecked, setChecked] = useState(true); // 
+  const [isChecked, setChecked] = useState(true); //
 
   const handleType = (e) => {
     setExpenseName(e.target.value);
@@ -98,6 +100,7 @@ function App() {
       setIncome(incomes.concat(incomeObject));
       setMoney("");
       setExpenseName("");
+      setMovements(movements.concat(incomeObject));
     } else {
       const expenseObject = {
         description: expenseName,
@@ -107,6 +110,7 @@ function App() {
       setExpense(expenses.concat(expenseObject));
       setMoney("");
       setExpenseName("");
+      setMovements(movements.concat(expenseObject));
     }
   };
 
@@ -118,24 +122,33 @@ function App() {
     setChecked(!isChecked);
   };
 
+    
+
   return (
-    <div className="App">
-      <h1>Stingy!</h1>
-      <h2>Total movements:</h2>
-      <Total incomes={getIncomes} expenses={getExpenses} />
-      <h2>Add new movement:</h2>
-      <SubmitForm
-        typeName={expenseName}
-        moneyNumber={money}
-        moneyChange={handleMoney}
-        checked={isChecked}
-        handleChange={handleChange}
-        typeChange={handleType}
-        submit={addNew}
-      />
-      <div className="movements">
-      <Movements movementType={expenses} className='expenses' />
-      <Movements movementType={incomes} className='incomes' />
+    
+    <div className='d'>
+      <div className="header">
+        <h1>Stingy!</h1>
+      </div>
+
+      <div className="App">
+        <h2>Add new movement:</h2>
+        <SubmitForm
+          typeName={expenseName}
+          moneyNumber={money}
+          moneyChange={handleMoney}
+          checked={isChecked}
+          handleChange={handleChange}
+          typeChange={handleType}
+          submit={addNew}
+        />
+        <div className="movements">
+          <Movements movementType={movements} className="movDetail" />
+        </div>
+        <div>
+          <h2>Total movements:</h2>.
+          <Total incomes={getIncomes} expenses={getExpenses} />
+        </div>
       </div>
     </div>
   );
